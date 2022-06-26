e347bc88da899f918bc78ece7a43eda8490bcc32
tekashiZiinMaKer <98004421+tekashiZiinMaKer@users.noreply.github.com>
2022-06-26 11:28:22 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#! /bin/bash 
+
+#Generator CCs _
+# Version: 1.0
+# Description: Generator CC's, A Simple Script That Lets You Generate CC's! 
+# Author: tekashiZiinMaKer
+# Github: https://github.com/tekashiZiinMaKer
+# E-mail: tekashimaker504@gmail.com
+# Crédits: tekashiZiinMaKer
+# Date: 22-02-2022
+# Language: Python
+# Portable: file 
+# Sif you copy, consider giving credit! We keep our code  open source to help others
+
+import sys
+import os
+import time
+import socket
+import random
+
+#start
+os.sys('clear')
+os.sys('figlet miau')
